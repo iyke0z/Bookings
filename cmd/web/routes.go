3381f0f94cb49412550f60b8626138fcf8389fb6
ikebeelion <ianikwe@next-itservices.com>
2022-06-14 17:41:12 +0100

    images

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -11,17 +11,25 @@ import (
 )
 
 func routes(app *config.AppConfig) http.Handler{
-	
+	//chiRouterPackage Initialize
 	mux := chi.NewRouter()
- 
+	
+	//A bunch of imported middlewares
 	mux.Use(middleware.Recoverer)
 	mux.Use(WriteToConsole)
+	//CSRF Middle ware created by me
 	mux.Use(NoSurve)
+	//CSRF Middle ware created by me to load session
 	mux.Use(SessionLoad)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 
+	//start of loading static files
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+	//end of loading static files
+	
 	return mux
 }
 
